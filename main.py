8c674797a2cc086161c1911400ad3350c0f03940
Hazel Viswanath <viswanath.hazel@gmail.com>
2024-12-19 13:50:58 -0700

    Draw Player to screen

--- main.py
+++ main.py
@@ -2,8 +2,20 @@
 import tcod
 
 
-def main():
-    print("Hello World")
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    tileset = tcod.tileset.load_tilesheet("sheet.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
+
+    with tcod.context.new_terminal(screen_width, screen_height, tileset=tileset, title="Dungeon Game", vsync=True) as context:
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=1, y=1, string="@")
+            context.present(root_console)
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
 
 
 if __name__ == "__main__":
