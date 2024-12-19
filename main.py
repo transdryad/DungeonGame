00b3e5174f91f9ea7dd0ba2568fb0aeaf7fff4f1
Hazel Viswanath <viswanath.hazel@gmail.com>
2024-12-19 16:35:33 -0700

    Engine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env/ python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
 from input_handlers import Eventhandler
 
@@ -17,20 +17,14 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
     with tcod.context.new_terminal(screen_width, screen_height, tileset=tileset, title="Dungeon Game", vsync=True) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
-            context.present(root_console)
-            root_console.clear()
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-                if action is None:
-                    continue
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx, dy=action.dy)
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
