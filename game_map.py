688ab85966e8c996703898224551c92c75e19113
Hazel Viswanath <viswanath.hazel@gmail.com>
2024-12-22 15:10:04 -0700

    FOV

--- game_map.py
+++ game_map.py
@@ -9,9 +9,15 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         self.tiles[30:33, 22] = tile_types.wall
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console):
-        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        console.rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
--- tile_types.py
+++ tile_types.py
@@ -2,12 +2,17 @@ from typing import Tuple
 import numpy as np
 
 graphic_dt = np.dtype([("ch", np.int32), ("fg", "3B"), ("bg", "3B")])
-tile_dt = np.dtype([("walkable", np.bool), ("transparent", np.bool), ("dark", graphic_dt)])
+tile_dt = np.dtype([("walkable", np.bool), ("transparent", np.bool), ("dark", graphic_dt), ("light", graphic_dt), ])
 
 
-def new_tile(*, walkable: int, transparent: int, dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+def new_tile(*, walkable: int, transparent: int, dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+             light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]], ) -> np.ndarray:
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
 
-floor = new_tile(walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),)
-wall = new_tile(walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),)
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
+
+floor = new_tile(walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+                 light=(ord(" "), (255, 255, 255), (200, 180, 50)),)
+wall = new_tile(walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+                light=(ord(" "), (255, 255, 255), (130, 110, 50)),)
